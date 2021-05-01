39bfaf02872cf8f0c4342a164a7fd54360b91df0
Minka <sackominkailou34@gmail.com>
2021-05-01 14:37:05 +0200

    NouveauProjet

---
+++ header.inc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+ $variable ="PHP";
+?>
+<html lang="fr">
+    <head> 
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <link href="style.css" rel="stylesheet" />
+        <link href="image/Msdesign.jpg" rel="icon"/>
+    </head>
+    <body>
+        <?php
+        echo"<ul class=\"navigation\" role=\"navigation\">
+            <img class=\"logo\"src=\"image/Msdesign2.jpg\" alt=\"logo\" />
+            <li><a href=\"#\"><b>A</b>ccueil</a></li>
+            <li><a href=\"#\"><b>F</b>ormulaire</a></li>
+            <li><a href=\"#\"><b>H</b>istorique</a></li>
+            <li><a href=\"#\"><b>C</b>ontact</a></li>
+            </ul>";
+        ?>
+        
+
+        <footer>
+       
+        </footer>
+
+    </body>
+</html>
